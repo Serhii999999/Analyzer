ed257c1437bc7374164348022ecccfef9c2f4ea7
Serhii <serhii.maletych@testmatick.com>
2024-05-02 15:31:43 +0300

    rework

---
+++ src/test/java/tests/jenkins/CheckLogTests1.java
@@ -0,0 +1,11 @@
+package tests.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckLogTests1 {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+}
